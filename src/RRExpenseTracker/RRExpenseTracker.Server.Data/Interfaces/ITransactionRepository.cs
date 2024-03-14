51b3c8789d9b8c8efcd8d5f6d135ac324af7040f
RAHUL26R <rahulroy.profile@gmail.com>
2024-03-14 11:44:15 +0530

    Implemented Transaction cosmosdb repository

---
+++ src/RRExpenseTracker/RRExpenseTracker.Server.Data/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace RRExpenseTracker.Server.Data.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task DeleteAsync(Transaction transaction);
+        Task CreateAsync(Transaction transaction);
+        Task UpdateAsync(Transaction transaction);
+    }
+}
---
+++ src/RRExpenseTracker/RRExpenseTracker.Server.Data/Repositories/CosmosTransactionRepository.cs
@@ -0,0 +1,49 @@
+﻿namespace RRExpenseTracker.Server.Data.Repositories
+{
+    public class CosmosTransactionRepository : ITransactionRepository
+    {
+
+        private readonly CosmosClient _cosmosClientDb;
+        private const string DATABASE_NAME = "ExpenseTrackerDb";
+        private const string CONTAINER_NAME = "Transactions";
+
+        private readonly Container _container;
+
+
+        public CosmosTransactionRepository(CosmosClient cosmosClientDb)
+        {
+            _cosmosClientDb = cosmosClientDb;
+            _container = _cosmosClientDb.GetContainer(DATABASE_NAME, CONTAINER_NAME);
+        }
+
+        public async Task CreateAsync(Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                throw new ArgumentNullException(nameof(transaction));
+            }
+
+            await _container.CreateItemAsync(transaction);
+        }
+
+        public async Task DeleteAsync(Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                throw new ArgumentNullException(nameof(transaction));
+            }
+
+            await _container.DeleteItemAsync<Transaction>(transaction.Id, new PartitionKey(transaction.UserIdYear));
+        }
+
+        public async Task UpdateAsync(Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                throw new ArgumentNullException(nameof(transaction));
+            }
+
+            await _container.ReplaceItemAsync<Transaction>(transaction, transaction.Id);
+        }
+    }
+}
