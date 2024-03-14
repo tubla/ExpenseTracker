5c75c2298350e013f4d230f2821483e185626e28
RAHUL26R <rahulroy.profile@gmail.com>
2024-03-14 12:35:32 +0530

    Created TransactionDto and TransactionDtoValidator

---
+++ src/RRExpenseTracker/RRExpenseTracker.Shared/DTOs/TransactionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace RRExpenseTracker.Shared.DTOs
+{
+    public class TransactionDto
+    {
+        public string? Id { get; set; }
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime? DateTime { get; set; }
+        public string Category { get; set; } = "Other";
+        public string[]? Tags { get; set; }
+        public string[]? Attachments { get; set; }
+        public bool IsIncome { get; set; }
+        public string WalletId { get; set; } = string.Empty;
+    }
+}
---
+++ src/RRExpenseTracker/RRExpenseTracker.Shared/Validators/TransactionDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RRExpenseTracker.Shared.DTOs;
+
+namespace RRExpenseTracker.Shared.Validators
+{
+    public class TransactionDtoValidator : AbstractValidator<TransactionDto>
+    {
+        public TransactionDtoValidator()
+        {
+            RuleFor(x => x.Amount).GreaterThanOrEqualTo(0).WithMessage("Amount cannot be negative");
+            RuleFor(x => x.WalletId).NotEmpty().WithMessage("Wallet is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.DateTime).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Date cannnot be in the future").When(x => x.DateTime != null);
+
+        }
+    }
+}
